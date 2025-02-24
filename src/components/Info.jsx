aa1774a8ba604a972ba750b896f6f61d5b49dcfe
Gideon1107 <ayenigideon1107@gmail.com>
2025-02-24 13:45:41 +0000

    Edit: Deploy

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ const Info = () => {
        <div className="flex flex-col gap-10">
             <h1 className="text-4xl font-medium text-white lg:pl-10 max-sm:text-center p-1">LET ME <span className="text-[#76d32e]">INTRODUCE</span> MYSELF</h1>
             <div className="flex flex-col gap-5 text-xl">
-                <p className="">I fell in love with programming during my University degree and I have taught something, I think… 🤷‍♂️</p>
+                <p className="">I fell in love with programming during my University degree and I have taught myself something, I think… 🤷‍♂️</p>
                 <p>I am proficient in <span className="text-[#76d32e] font-semibold italic">Python, Javascript and Typescript</span>.</p>
                 <p>Passionate about building <span className="text-[#76d32e] font-semibold italic ">Scalable Software Solutions</span> and <span className="text-[#76d32e] font-semibold italic">APIs.</span></p>
                 <p></p>
